35db6050f690d44d8a46f188b846fb89426c5dc2
EdilvaCarvalho <edilva.intin@gmail.com>
2022-08-30 09:08:34 -0300

    mudanças

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'biblioteca-django',
         'USER': 'postgres',
-        'PASSWORD': '123456',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
