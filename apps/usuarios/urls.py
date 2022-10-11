3538323a304afc4bb5168a570300341b29e671c5
EdilvaCarvalho <edilva.intin@gmail.com>
2022-10-11 08:44:43 -0300

    CRUD de Usuário e Login

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('criar/usuario/', UsuarioCreate.as_view(), name='criar_usuario'),
 ]
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,9 +1,9 @@
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
+from .form import UsuarioForm
 
 class UsuarioCreate(CreateView):
-    model = User
-    fields = '__all__'
+    form_class = UsuarioForm
     template_name = "cadastros/form.html"
     success_url = reverse_lazy("index")
